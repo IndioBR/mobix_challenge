53641a05f18afc52202d7d5cfd8fc9a3c2a6c424
Jonatas Ximenez <88745612+IndioBR@users.noreply.github.com>
2022-09-16 16:33:13 -0400

    'Complicações surgiram, continuaram e foram superadas' - Sparrow, Capitão Jack.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const BREAK_LINE = '\n';
 const SPACE = ' ';
 
-const compass = (n) => {
-  const FULL_LINE = '#'.repeat(n) + BREAK_LINE;
+const compass = (n = 20, character = '#') => {
+  const FULL_LINE = character.repeat(n) + BREAK_LINE;
   let triangle = '';
   let emptyTriangle = '';
   let side = 0;
 
 
   for (i = 0; i < Math.floor(n / 2); i++) {
+    triangle += character + SPACE.repeat(i) + character.repeat(n - (2 + (i + i))) + SPACE.repeat(i) + character;
+    side = i;
     if (i <= Math.floor(n / 2)) {
-      triangle += '#' + SPACE.repeat(i) + '#'.repeat(n - (2 + (i + i))) + SPACE.repeat(i) + '#' + BREAK_LINE;
-      side = i;
+      triangle += BREAK_LINE;
     } else {
       break;
     }
@@ -19,18 +20,18 @@ const compass = (n) => {
 
   for (i = 1; i < Math.floor(n / 2); i += 1) {
     if (n % 2 === 0) {
-      emptyTriangle += '#' + SPACE.repeat(side - i) + '#' + SPACE.repeat(i + (i - 2)) + '#' + SPACE.repeat(side - i) + '#' + BREAK_LINE;
+      emptyTriangle += character + SPACE.repeat(side - i) + character + SPACE.repeat(i + (i - 2)) + character + SPACE.repeat(side - i) + character + BREAK_LINE;
     } else if (n % 2 !== 0) {
-      emptyTriangle += '#' + SPACE.repeat(side - i) + '#' + SPACE.repeat(i + (i - 1)) + '#' + SPACE.repeat(side - i) + '#' + BREAK_LINE;
+      emptyTriangle += character + SPACE.repeat(side - i) + character + SPACE.repeat(i + (i - 1)) + character + SPACE.repeat(side - i) + character + BREAK_LINE;
     } else {
       break;
     }
   }
 
-
-  const FINAL = triangle + emptyTriangle
+  let fullTriangle = triangle.replace(character + SPACE.repeat(n - 2) + character, '').trim();
+  const FINAL = fullTriangle + BREAK_LINE + emptyTriangle;
 
   return FULL_LINE + FINAL + FULL_LINE;
 }
 
-console.log(compass(100));
+console.log(compass(100, '@'));
